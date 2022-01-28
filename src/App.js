1b4977283a4a8f8f7b232076fef131139ed35dda
FotygaMateusz <informatyka.mateusz@gmail.com>
2022-01-28 14:02:41 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./Music.css";
+import Player from "./components/Player";
+import song1img from "./images/song1.jpg";
+import song2img from "./images/song2.jpg";
+import song3img from "./images/song3.jpg";
+import song1 from "./songs/song1.mp3";
+import song2 from "./songs/song2.mp3";
+import song3 from "./songs/song3.mp3";
 
-function App() {
+const Music = (props) => {
+  const [songs] = useState([
+    {
+      title: "Creative Minds",
+      artist: "Benjamin Tissot",
+      img_src: song1img,
+      src: song1,
+    },
+    {
+      title: "Happy Rock",
+      artist: "Bensound",
+      img_src: song2img,
+      src: song2,
+    },
+    {
+      title: "Hey!",
+      artist: "Benjamin Tissot",
+      img_src: song3img,
+      src: song3,
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Music">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
-}
+};
 
-export default App;
+export default Music;
